8d7541f4fabc8df79c49511d69e60eee309d24cc
nou44 <draouiy22@gmail.com>
2026-01-15 23:08:40 +0100

    pleaz

--- market.js
+++ market.js
@@ -1,6 +1,18 @@
+const CONTRACT_ADDRESS = "0x8164214b395b68Bc2BAe5F38728E7c790066E7b8";
+const ABI = [
+  "function mint(string uri) payable"
+];
 
+let provider, signer, contract;
 
-const grid = document.getElementById("marketGrid");
+async function connectWallet(){
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
+  signer = provider.getSigner();
+  contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+}
+
+connectWallet();
 
 fetch("data/nfts.json")
   .then(res => res.json())
@@ -18,23 +30,41 @@ fetch("data/nfts.json")
         </div>
         <div class="nft-info">
           <h3>${nft.name} #${nft.id}</h3>
-          <span>Price: ${nft.price}</span>
-          <a href="${nft.rarible}" target="_blank">
-            <button class="mint-btn">Mint</button>
-          </a>
+          <span>Price: 200 POL</span>
+          <button class="mint-btn" data-id="${nft.id}">Mint</button>
         </div>
       `;
 
       const video = card.querySelector("video");
+      const mintBtn = card.querySelector(".mint-btn");
 
       card.addEventListener("mouseenter", () => video.play());
       card.addEventListener("mouseleave", () => video.pause());
+
+      mintBtn.onclick = async () => {
+        try {
+          const uri = `https://market.moonees.org/metadata/${nft.id}.json`;
+
+          const tx = await contract.mint(uri, {
+            value: ethers.utils.parseEther("200")
+          });
+
+          mintBtn.innerText = "Minting...";
+          await tx.wait();
+          mintBtn.innerText = "Minted ✔";
+
+        } catch (err) {
+          console.error(err);
+          alert("Mint failed");
+        }
+      };
 
       grid.appendChild(card);
 
     });
 
   });
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
